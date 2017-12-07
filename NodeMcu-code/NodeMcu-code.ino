efdd68abfcc8874efe8f329ea9dab4f8ff005cac
Стоян Гълъбов <stoqn_2409@abv.bg>
2017-12-07 18:34:18 +0200

    Update NodeMcu-code.ino

--- NodeMcu-code/NodeMcu-code.ino
+++ NodeMcu-code/NodeMcu-code.ino
@@ -1,1 +1,123 @@
+ /*
+MIT License
+Copyright (c) 2017 Стоян Гълъбов
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */ 
+//You need to add these libraries to can your code work.
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 
+// Update these with values suitable for your network.
+const char* ssid = "**************";
+const char* password = "*************";
+const char* mqtt_server = "****************";
+int led = 2;
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+int const SEND_DELAY = 2000;
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network.
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    digitalWrite(led, LOW);   // Turn the LED on (Note that LOW is the voltage level
+  }  
+  if ((char)payload[0] == '2') {
+    digitalWrite(led, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("wi-fi led", "hello world");
+      // ... and resubscribe
+      client.subscribe("wi-fi led");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  pinMode(led, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void loop() {
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  long now = millis();
+  if (now - lastMsg > SEND_DELAY) { // If "now" - "lastMsg" is bigger than 2000 =>
+    lastMsg = now;                            // "lastMsg" is equally to "now". "2000" is value.
+    ++value;
+    snprintf (msg, 75, "hello world #%ld", value); 
+    Serial.print("Publish message: ");             
+    Serial.println(msg);
+    client.publish("wi-fi led", msg); 
+  }
+}
