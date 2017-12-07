238362adf5d0ddfb43453d0716043e80dca63fc5
Стоян Гълъбов <stoqn_2409@abv.bg>
2017-12-07 17:25:59 +0200

    Comments added

--- NodeMcu-code.ino
+++ NodeMcu-code.ino
@@ -21,7 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
-*/ 
+ */ 
 //You need to add these libraries to can your code work.
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
